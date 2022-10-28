5a104224167cbe74fab86a6cada3e9af3edc6051
Isaac Adorno <isaacadorno21@gmail.com>
2022-10-28 07:51:50 -0500

    format set for future problems

--- Arrays_and_Hashing/src/contains_duplicate.py
+++ Arrays_and_Hashing/src/contains_duplicate.py
@@ -1,7 +1,5 @@
-'''
-Problem Description:
-- Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
--------------------------------------
+"""Problem Description: - Given an integer array nums, return true if any value appears at least twice in the array,
+and return false if every element is distinct.
 Example 1:
 
 Input: nums = [1,2,3,1]
@@ -16,20 +14,29 @@ Example 3:
 
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
--------------------------------------
-'''
+"""
 
 from collections import Counter
 
 
 class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool:
+    @staticmethod
+    def contains_duplicate(nums: list[int]) -> bool:
         nums_counter = Counter(nums)
         if any(value > 1 for value in nums_counter.values()):
             return True
         return False
 
-solution = Solution()
-assert solution.containsDuplicate([1,2,3,1]) == True
-assert solution.containsDuplicate([1,2,3,4]) == False
-assert solution.containsDuplicate([1,1,1,3,3,4,3,2,4,2]) == True
+
+'''
+Notes on the solution:
+- I used a Counter here to keep track of the values of the array, and the number of times they occurred
+
+Other solutions: 
+- We could convert this to a set and check if it's the same length 
+    - If it is the same length, we know there's no duplicates 
+    - If it isn't, we know there's a problem. The set could be smaller because elements 
+      might have been rejected if they already existed in the set
+- We could use a regular Python dictionary and store the counts ourselves
+- We could also sort and see if any values if any consecutive elements are equal
+'''
--- Arrays_and_Hashing/tests/test_contains_duplicate.py
+++ Arrays_and_Hashing/tests/test_contains_duplicate.py
@@ -1,24 +1,26 @@
 import unittest
-from src.contains_duplicate import Solution
+from Arrays_and_Hashing.src.contains_duplicate import Solution
+
 
 class TestSolution(unittest.TestCase):
     def test_1(self):
-        nums = [1,2,3,1]
+        nums = [1, 2, 3, 1]
         expected = True
-        actual = Solution().containsDuplicate(nums)
+        actual = Solution().contains_duplicate(nums)
         self.assertEqual(expected, actual)
 
     def test_2(self):
-        nums = [1,2,3,4]
+        nums = [1, 2, 3, 4]
         expected = False
-        actual = Solution().containsDuplicate(nums)
+        actual = Solution().contains_duplicate(nums)
         self.assertEqual(expected, actual)
 
     def test_3(self):
-        nums = [1,1,1,3,3,4,3,2,4,2]
+        nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
         expected = True
-        actual = Solution().containsDuplicate(nums)
+        actual = Solution().contains_duplicate(nums)
         self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
     unittest.main()
